4539c45189f176bf543b5c5d33a365cf4882615f
EricNg33192 <145098701+EricNg33192@users.noreply.github.com>
2023-12-07 13:57:54 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors())
+
+app.use(express.json());   
+app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post("/upload-file", upload.single('file'), (req, res) => {
+    fs.writeFileSync(`./picture/${req.body.id}-${req.file.originalname}`,  req.file.buffer);
+    res.end();
+})
+
+app.use((req, res) => {
+    res.status(404) 
+    res.json({error: "Page Not Found"})
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500);
+    res.json({error: "500 Internal Server Error"})
+});
+
+app.listen(3000, () => {
+    console.log("Server started successfully")
+});
